08cb12fc7987cd40476d709981762d96792aa9fd
clicamal <justother94@gmail.com>
2024-10-24 12:29:59 -0400

    Refatorada gdt_set_entry

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -7,19 +7,21 @@ static struct gdt_entry gdt[GDT_ENTRIES_COUNT];
 static struct gdt_ptr gdt_p;
 
 void gdt_set_entry(int index, uint32_t base, uint32_t limit, uint8_t access, uint8_t granularity) {
-  gdt[index].base_low = (base & 0xFFFF);
-  gdt[index].base_mid = (base >> 16) & 0xFF;
-  gdt[index].base_high = (base >> 24) & 0xFF;
+  struct gdt_entry *entry = &gdt[index];
 
-  gdt[index].limit_low = (limit & 0xFFFF);
-  gdt[index].granularity = (limit >> 16) & 0x0F;
+  entry->base_low = (base & 0xFFFF);
+  entry->base_mid = (base >> 16) & 0xFF;
+  entry->base_high = (base >> 24) & 0xFF;
 
-  gdt[index].granularity |= granularity & 0xF0;
-  gdt[index].access = access;
+  entry->limit_low = (limit & 0xFFFF);
+  entry->granularity = (limit >> 16) & 0x0F;
+
+  entry->granularity |= granularity & 0xF0;
+  entry->access = access;
 }
 
 void gdt_init(void) {
-  gdt_p.limit = (sizeof(struct gdt_entry) * GDT_ENTRIES_COUNT) - 1;
+  gdt_p.limit = sizeof(gdt) - 1;
   gdt_p.base = (uint32_t) &gdt;
 
   gdt_set_entry(0, 0, 0, 0, 0);
