909f1eebd634d3e2ada0378bf448c715fab973ba
clicamal <justother94@gmail.com>
2024-10-24 12:27:45 -0400

    Simplificada lógica de semáforo

--- kernel/arch/i386/semaphore.c
+++ kernel/arch/i386/semaphore.c
@@ -1,38 +1,18 @@
-#include <kernel/queue.h>
 #include <kernel/semaphore.h>
 #include <stdlib.h>
 
 void semaphore_init(struct semaphore *sem) {
   sem->locked = false;
-  queue_init(sem->queue);
 }
 
-void semaphore_enqueue(struct semaphore *sem, void *item) {
-  enqueue(sem->queue, item);
+void semaphore_wait(struct semaphore *sem) {
+  while (sem->locked);
 }
 
-void semaphore_dequeue(struct semaphore *sem) { dequeue(sem->queue); }
+void semaphore_take(struct semaphore *sem) {
+  semaphore_wait(sem);
 
-void *semaphore_queue_peek(struct semaphore *sem) {
-  return queue_peek(sem->queue);
+  sem->locked = true;
 }
-
-void semaphore_take(struct semaphore *sem) { sem->locked = true; }
 
 void semaphore_rls(struct semaphore *sem) { sem->locked = false; }
-
-void semaphore_cycle(struct semaphore *sem) {
-  if (!sem->locked) {
-    void (*obs_callback)(void) = semaphore_queue_peek(sem);
-
-    if (obs_callback != NULL) {
-      semaphore_take(sem);
-
-      obs_callback();
-
-      semaphore_dequeue(sem);
-
-      semaphore_rls(sem);
-    }
-  }
-}
--- kernel/include/kernel/semaphore.h
+++ kernel/include/kernel/semaphore.h
@@ -5,16 +5,12 @@
 #include <stdbool.h>
 
 struct semaphore {
-  struct queue *queue;
   bool locked;
 };
 
 void semaphore_init(struct semaphore *sem);
-void semaphore_enqueue(struct semaphore *sem, void *item);
-void semaphore_dequeue(struct semaphore *sem);
-void *semaphore_queue_peek(struct semaphore *sem);
+void semaphore_wait(struct semaphore *sem);
 void semaphore_take(struct semaphore *sem);
 void semaphore_rls(struct semaphore *sem);
-void semaphore_cycle(struct semaphore *sem);
 
 #endif
