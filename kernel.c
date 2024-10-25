6e399df63e017df258c4e00957e1742d8806e4de
clicamal <justother94@gmail.com>
2024-10-24 20:26:12 -0400

    Modificados kernel.c e default_int.c

--- kernel.c
+++ kernel.c
@@ -1,43 +1,43 @@
 #include <kernel/drivers/kbd.h>
 #include <kernel/pic.h>
 #include <kernel/idt.h>
-#include <kernel/tty.h>
+#include <kernel/drivers/vga.h>
 #include <kernel/gdt.h>
 #include <stdbool.h>
 
 void kernel_main(void) {
-  term_init();
-  term_write_str("Hello from Magikaed-OS kernel!\n");
+  vga_init();
+  vga_write_str("Hello from Magikaed-OS kernel!\n");
 
-  term_write_str("Loading GDT...\n");
+  vga_write_str("Loading GDT...\n");
 
   gdt_init();
 
-  term_write_str("GDT Loaded!\n");
+  vga_write_str("GDT Loaded!\n");
 
-  term_write_str("Loading IDT...\n");
+  vga_write_str("Loading IDT...\n");
 
   idt_init();
 
-  term_write_str("IDT Loaded...\n");
+  vga_write_str("IDT Loaded...\n");
 
-  term_write_str("Initializing PIC...\n");
+  vga_write_str("Initializing PIC...\n");
 
   pic_init();
 
-  term_write_str("PIC Initialized!\n");
+  vga_write_str("PIC Initialized!\n");
 
-  term_write_str("Initializing Keyboard Driver...\n");
+  vga_write_str("Initializing Keyboard Driver...\n");
 
   kbd_init();
 
-  term_write_str("Keyboard Driver Initialized. You can start typing!\n");
+  vga_write_str("Keyboard Driver Initialized. You can start typing!\n");
 
   while (true) {
     char buffer[256];
 
     kbd_read(buffer, 256);
 
-    term_write_str(buffer);
+    vga_write_str(buffer);
   }
 }
