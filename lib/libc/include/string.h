2250b7065f50bb33277d2c8716ec306dd2c0139e
clicamal <justother94@gmail.com>
2024-11-02 21:09:44 -0400

    Adicionadas funções strncmp e strncpy

--- lib/libc/include/string.h
+++ lib/libc/include/string.h
@@ -5,6 +5,8 @@
 
 size_t strlen(const char *);
 void *memcpy(void *, const void *, size_t n);
+int strncmp(const char *str1, const char *str2, size_t n);
 int strcmp(const char *str1, const char *str2);
+char *strncpy(char *dest, const char *src, size_t n);
 
 #endif
---
+++ lib/libc/string/strncmp.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stddef.h>
+
+int strncmp(const char *str1, const char *str2, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    if (str1[i] != str2[i] || str1[i] == '\0' || str2[i] == '\0') {
+      return (unsigned char)str1[i] - (unsigned char)str2[i];
+    }
+  }
+
+  return 0;
+}
