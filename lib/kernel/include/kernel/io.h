28f560dd1f8a959e6a1e4906ccf144892df593d8
clicamal <justother94@gmail.com>
2024-11-03 22:48:05 -0400

    Removido modificador static de outb/inb e adicionados inw/outw

--- lib/kernel/include/kernel/io.h
+++ lib/kernel/include/kernel/io.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-static inline uint8_t inb(uint16_t port) {
+inline uint8_t inb(uint16_t port) {
   uint8_t res;
 
   asm volatile ("inb %w1, %b0" : "=a" (res) : "Nd" (port) : "memory");
@@ -11,11 +11,23 @@ static inline uint8_t inb(uint16_t port) {
   return res;
 }
 
-static inline void outb(uint16_t port, uint8_t data) {
+inline void outb(uint16_t port, uint8_t data) {
   asm volatile ("outb %b0, %w1" : : "a" (data), "Nd" (port) : "memory");
 }
 
-static inline void io_wait(void) {
+inline uint16_t inw(uint16_t port) {
+  uint16_t res;
+
+  asm volatile("inw %w1, %w0" : "=a"(res) : "Nd"(port) : "memory");
+
+  return res;
+}
+
+inline void outw(uint16_t port, uint16_t data) {
+  asm volatile("outw %w0, %w1" : : "a"(data), "Nd"(port) : "memory");
+}
+
+inline void io_wait(void) {
   outb(0x80, 0);
 }
 
